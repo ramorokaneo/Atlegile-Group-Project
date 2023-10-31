172d360a54460f34f52aa39ecaab310040fab9c9
Ristar-art <mochochokoboiketlo@gmail.com>
2023-10-31 13:34:20 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,101 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import ManageUserSceen from './ManageUserScreen';
+import picture from "./GroupAtlegileUsers.png";
 import "./App.css";
 function App() {
+
+  function calculateBackgroundProperties() {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    let backgroundPositionX, backgroundPositionY, backgroundSize;
+
+    // Define the standard screen sizes and their corresponding values
+    const screenSizes = [
+      {
+        width: 1515,
+        height: 1441,
+        positionX: 340,
+        positionY: -150,
+        size: "700px 450px",
+      },
+      {
+        width: 1738,
+        height: 871,
+        positionX: 530,
+        positionY: -115,
+        size: "600px 350px",
+      },
+      // Add more screen sizes and values as needed
+    ];
+
+    // Calculate the ratio of the screen size to the standard sizes
+    const matchingSize = screenSizes.find(
+      (size) => size.width === screenWidth && size.height === screenHeight
+    );
+
+    if (matchingSize) {
+      backgroundPositionX = matchingSize.positionX;
+      backgroundPositionY = matchingSize.positionY;
+      backgroundSize = matchingSize.size;
+    } else {
+      // Calculate ratios for custom screen sizes
+      const ratioX = screenWidth / screenSizes[0].width;
+      const ratioY = screenHeight / screenSizes[0].height;
+
+      backgroundPositionX = Math.round(screenSizes[0].positionX * ratioX);
+      backgroundPositionY = Math.round(screenSizes[0].positionY * ratioY);
+      backgroundSize = `${Math.round(700 * ratioX)}px ${Math.round(
+        450 * ratioY
+      )}px`;
+    }
+
+    return { backgroundPositionX, backgroundPositionY, backgroundSize };
+  }
+
+  const { backgroundPositionX, backgroundPositionY, backgroundSize } = calculateBackgroundProperties();
+
+  const data = [
+    {
+      name: "Jane",
+      surname: "Doe",
+      phone: "0123456789",
+      email: "example@gmail.com",
+      location: "1235 Vilakazi street...",
+      actions: ["Block User", "View Details"],
+    },
+    {
+      name: "Jane",
+      surname: "Doe",
+      phone: "0123456789",
+      email: "example@gmail.com",
+      location: "1235 Vilakazi street...",
+      actions: ["Block User", "View Details"],
+    },
+    {
+      name: "Jane",
+      surname: "Doe",
+      phone: "0123456789",
+      email: "example@gmail.com",
+      location: "1235 Vilakazi street...",
+      actions: ["Block User", "View Details"],
+    },
+    {
+      name: "Jane",
+      surname: "Doe",
+      phone: "0123456789",
+      email: "example@gmail.com",
+      location: "1235 Vilakazi street...",
+      actions: ["Block User", "View Details"],
+    },
+    {
+      name: "Jane",
+      surname: "Doe",
+      phone: "0123456789",
+      email: "example@gmail.com",
+      location: "1235 Vilakazi street...",
+      actions: ["Block User", "View Details"],
+    },
+    // Add more data objects as needed
+  ];
   return (
     <div className="App">
       <div className="sidenav">
@@ -53,7 +146,16 @@ function App() {
           <div className="managerheaderText">
             <h1>Manage Users</h1>
           </div>
-          <div className="managerheaderIcon"></div>
+          <div style={{
+              backgroundImage: `url(${picture})`,
+              width: "60%",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: `${backgroundPositionX}px ${backgroundPositionY}px`,
+              backgroundSize: backgroundSize,
+              height: "100%",
+            }}>
+          
+          </div>
         </div>
         <div className="managerContent">
           <div className="contentContaner">
@@ -84,18 +186,69 @@ function App() {
                   </div>
                   <div className="NewUserNumber">
                     <p>300</p>
-                  </div>                
-                </div>                
+                  </div>
+                </div>
               </div>
-              <div className="space"></div>
+              {/* <div className="space"></div> */}
 
-              <div className = "NewUser">
-              <div className="usersheader">
-                <h1>New Users</h1>
-              </div>
+              <div className="NewUser">
+                <div className="usersheader">
+                  <h1>New Users</h1>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="table">
+          <div className="headings">
+            <div className="heading">
+              <p>Name</p>
+            </div>
+            <div className="heading">
+              <p>Surname</p>
+            </div>
+            <div className="heading">
+              <p>Phone</p>
+            </div>
+            <div className="heading">
+              <p>Email</p>
+            </div>
+            <div className="heading">
+              <p>Location</p>
+            </div>
+            <div className="heading">
+              <p>Actions</p>
+            </div>
+          </div>
+        </div>
+        <div className="details">
+          {data.map((item, index) => (
+            <div className="headings1" key={index}>
+              <div className="heading1">
+                <p>{item.name}</p>
+              </div>
+              <div className="heading1">
+                <p>{item.surname}</p>
+              </div>
+              <div className="heading1">
+                <p>{item.phone}</p>
+              </div>
+              <div className="heading1">
+                <p>{item.email}</p>
+              </div>
+              <div className="heading1">
+                <p>{item.location}</p>
+              </div>
+              <div className="heading1">
+                <div className="heading2">
+                  <p>{item.actions[0]}</p>
+                </div>
+                <div className="heading2">
+                  <p>{item.actions[1]}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
