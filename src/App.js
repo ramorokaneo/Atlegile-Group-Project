97219ec510fe9467662436b15aac43f3216f4e36
Letlhogonolo01 <letlhogonolo.moroke8@gmail.com>
2023-10-31 10:59:45 +0200

    sign up screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
-// import UserSignUp from './UserSignUp/UserSignUp'
-import AltContact from './AltContact/AltContact';
+import UserSignUp from './UserSignUp/UserSignUp'
+// import AltContact from './AltContact/AltContact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <UserSignUp /> */}
-      <AltContact />
+      <UserSignUp />
+      {/* <AltContact /> */}
     </div>
   );
 }
