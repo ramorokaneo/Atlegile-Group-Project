399eccb7d7de6a5a620a6e108f5436ac2f02c5d6
Mbali <mbalikhumalo408@gmail.com>
2023-11-01 10:20:09 +0200

    about

--- src/LandingPageHeader/LandingPageHeader.js
+++ src/LandingPageHeader/LandingPageHeader.js
@@ -7,8 +7,9 @@ import { FiYoutube } from 'react-icons/fi';
 import { FiTwitter } from 'react-icons/fi';
 import { SiFacebook } from 'react-icons/si';
 import Logo from "../assets/Logo.png";
-import lion from "../assets/Lion.png"; 
-import shadow from "../assets/Shadow_Logo.png"
+import lion from "../assets/Lion.png";
+import shadow from "../assets/Shadow_Logo.png";
+import log from "../assets/Logo.png"
 
 
 
@@ -29,15 +30,33 @@ const LandingPageHeader = () => {
             </header>
 
             {/* next header */}
-            <img className="mid-header" src={Logo} alt=""/>
+            <img className="mid-header" src={Logo} alt="" />
             <button className="shop-button">SHOP</button>
             <button className="about-us">ABOUT US</button>
             <button className="sign-in">SIGN IN</button>
             <button className="sign-up">SIGN UP</button>
 
             <div className="lower-header">
-                <img className="lion" src={lion} alt=""/>
-                <img className="logo-2" src={shadow} alt=""/>
+                <img className="lion" src={lion} alt="" />
+                <img className="logo-2" src={shadow} alt="" />
+            </div>
+
+            <div className="p-tag">
+                <img src={log} alt="" />
+                <p className="market">ATLEGILE MARKETING SOLUTIONS</p>
+                <p>Welcome to Atlegile Marketing Solutions (Pty) Ltd, where passion, purpose, and expertise collide to create exceptional Marketing strategies. Our Youth<br />
+                    Woman-owned and led Business, based in South Africa, was founded in 2015, and we’ve been on an exciting journey with our Partners ever since.<br />
+                    From ground level to the Digital space, we’re committed to building strong Brands, effectively Communicating products + service offerings, and transferring our<br />
+                    Skills to help you engage with the Online Market Successfully.</p>
+                <p>AMS is dedicated to helping African businesses succeed by developing strong brands, marketing their products and services effectively, and connecting<br />
+                    them with a global customer base. They aim to drive sales, increase revenue, and create a lasting impact.
+                </p>
+            </div>
+
+            <div className="flexbox-container-1">
+                <div className="flexbox-item flexbox-item-1"/>
+                <div className="flexbox-item flexbox-item-2"/>
+                <div className="flexbox-item flexbox-item-3"/>
             </div>
 
         </div>
