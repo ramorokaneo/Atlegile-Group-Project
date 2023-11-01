6c07f4f53fc1d87910a065d0b8999f7bdc8f4b8e
Letlhogonolo01 <letlhogonolo.moroke8@gmail.com>
2023-11-01 10:09:37 +0200

    main account screen

--- src/MainAcc/MainAcc.js
+++ src/MainAcc/MainAcc.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainAcc.css";
+import { FaCaretDown } from "react-icons/fa";
 
 function UserSignUp() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [gender, setGender] = useState("");
+  const [email, setEmail] = useState("");
+  const [location, setLocation] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if any of the fields are empty
+    if (!name || !surname || !phone || !gender || !email || !location) {
+      alert("Please fill in all fields before continuing.");
+      return;
+    }
+
+    // Handle form submission logic here
+    console.log("Form submitted:", { name, surname, phone, gender, email, location });
+  };
+
   return (
     <div className="background-container">
       <div className="form-container">
@@ -9,28 +30,36 @@ function UserSignUp() {
           <img src="./logo.png" alt="Logo" className="logo" />
           <h3 className="main-acc">Main Account Holder</h3>
           <h2 className="yourself">TELL US ABOUT YOURSELF</h2>
-          <form>
-            <label>Name</label>
-            <input
-              type="text"
-              placeholder="Jane"
-              className="form-group"
-              required
-            />
+          <form onSubmit={handleSubmit}>
+            <div className="fullname">
+              <label className="label-1">Name</label>
+              <input
+                type="text"
+                placeholder="Jane"
+                className="form-group-1"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
 
-            <label>Surname</label>
-            <input
-              type="text"
-              placeholder="Doe"
-              className="form-group"
-              required
-            />
+              <label className="label-1">Surname</label>
+              <input
+                type="text"
+                placeholder="Doe"
+                className="form-group-1"
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
+                required
+              />
+            </div>
 
             <label>Phone</label>
             <input
               type="text"
               placeholder="0123456789"
               className="form-group"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
               required
             />
 
@@ -39,14 +68,19 @@ function UserSignUp() {
               type="text"
               placeholder=""
               className="form-group"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
               required
-            />
+              />
+              <FaCaretDown/>
 
             <label>Email</label>
             <input
               type="email"
               placeholder="example@gmail.com"
               className="form-group"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
 
@@ -55,6 +89,8 @@ function UserSignUp() {
               type="text"
               placeholder="1235 Vilakazi Street, Orlando West, Soweto, 1804, South Africa"
               className="form-group"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               required
             />
 
