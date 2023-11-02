c32ba12b1be3a7e279170383ce2633b6727a4f21
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-11-02 09:26:37 +0100

    searchBar

--- src/componets/NavBar/NavBar.jsx
+++ src/componets/NavBar/NavBar.jsx
@@ -1,56 +1,83 @@
+// import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faYoutube,
+//   faTwitter,
+//   faInstagram,
+//   faFacebook,
+// } from "@fortawesome/free-brands-svg-icons";
+// import "./NavBar.css";
+// import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+// import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar">
+//       <div className="contact-info">
+//         {/* <FontAwesomeIcon icon="fa-light fa-phone" className="fa-phone" /> */}
+//         <FontAwesomeIcon icon={faPhone} />
+//         <p> (225) 555-0118</p>
+//       </div>
+//       <div className="contact-info">
+//         <p>
+//           <FontAwesomeIcon icon={faEnvelope} />
+//           michelle.rivera@example.com
+//         </p>
+//       </div>
+//       <div className="contact-info follow-us1">
+//         <p>Follow us and get a chance to win 80% off</p>
+//       </div>
+//       <div className="follow-us">
+//         <p>Follow us :</p>
+//         <ul className="social-icons">
+//           <li className="navbar-item">
+//             <a href="https://www.instagram.com">
+//               <FontAwesomeIcon icon={faInstagram} />
+//             </a>
+//           </li>
+//           <li className="navbar-item">
+//             <a href="https://www.youtube.com">
+//               <FontAwesomeIcon icon={faYoutube} />
+//             </a>
+//           </li>
+
+//           <li className="navbar-item">
+//             <a href="https://www.facebook.com">
+//               <FontAwesomeIcon icon={faFacebook} />
+//             </a>
+//           </li>
+//           <li className="navbar-item">
+//             <a href="https://www.twitter.com">
+//               <FontAwesomeIcon icon={faTwitter} />
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faYoutube,
-  faTwitter,
-  faInstagram,
-  faFacebook,
-} from "@fortawesome/free-brands-svg-icons";
 import "./NavBar.css";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+// import logo from "./your-logo.png"; // Import your logo image
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="contact-info">
-        {/* <FontAwesomeIcon icon="fa-light fa-phone" /> */}
-        <FontAwesomeIcon icon={faPhone} />
-        <p> (225) 555-0118</p>
+      <div className="navbar-logo">
+        <img src="./images/logo.png" alt="Logo" />
       </div>
-      <div className="contact-info">
-        <p>
-          <FontAwesomeIcon icon={faEnvelope} />
-          michelle.rivera@example.com
-        </p>
-      </div>
-      <div className="contact-info follow-us1">
-        <p>Follow us and get a chance to win 80% off</p>
-      </div>
-      <div className="follow-us">
-        <p>Follow us :</p>
-        <ul className="social-icons">
-          <li className="navbar-item">
-            <a href="https://www.youtube.com">
-              <FontAwesomeIcon icon={faYoutube} />
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="https://www.twitter.com">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="https://www.instagram.com">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="https://www.facebook.com">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-          </li>
-        </ul>
+      <div className="navbar-buttons">
+        <div className="more-info">
+          <button className="btn">Shop</button>
+          <button className="btn">About Us</button>
+        </div>
+        <div className="login">
+          <button className="btn">Sign In</button>
+          <button className="btn">Sign Up</button>
+        </div>
       </div>
     </nav>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,56 +2,19 @@
 import React from "react";
 import "./landing.css";
 import Footer from "../componets/Footer/Footer";
+import FollowUs from "../componets/FollowUs/FollowUs";
+import SaveBigHeader from "../componets/SaveBigHeader/SaveBigHeader";
+import AfricaBusines from "../componets/AfricaBusiness/AfricaBusines";
 import Navbar from "../componets/NavBar/NavBar";
+import SearchBar from "../componets/SearchBar/SearchBar";
 function Landing() {
   return (
     <>
+      <FollowUs />
       <Navbar />
-      <div className="africa">
-        <div className="africa-container">
-          <div className="africa-hero">
-            <div className="africa-body">
-              <h1 className="africa-header">Africa's Business Support</h1>
-              <p className="africa-header-text">
-                High Impact Enter prise + Skills Development Solutions
-              </p>
-              <div className="africa-buttons">
-                <button className="btn">SHOP</button>
-                <button className="btn"> ABOUT US</button>
-              </div>
-            </div>
-            <div className="africa-cards">
-              <div className="card">
-                <p className="card-text">SoWhereTo &gt; Township Business</p>
-                <p className="card-text-btn">VIEW PROJECT</p>
-              </div>
-              <div className="card">
-                <p className="card-text">SoWhereTo &gt; Youth Training</p>
-                <p className="card-text-btn">VIEW PROJECT</p>
-              </div>
-              <div className="card">
-                {" "}
-                <p className="card-text">SoWhereTo &gt; @HUB Support</p>
-                <p className="card-text-btn">VIEW PROJECT</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="shop1-image-container">
-          <img
-            className="shop1-image"
-            src={"./images/shop1.png"}
-            alt="shop 1"
-          />
-        </div>
-        <div className="shop2-image-container">
-          <img
-            className="shop2-image"
-            src={"./images/shop2.png"}
-            alt="shop 2"
-          />
-        </div>
-      </div>
+      <SearchBar />
+      <SaveBigHeader />
+      <AfricaBusines />
       <Footer />
     </>
   );
