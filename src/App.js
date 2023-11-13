3435505d6edb49318af63e6667a907c00f6a6d0f
Mbaliii <mbalikhumalo408@gmail.com>
2023-11-13 17:20:33 +0200

    route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Register from './BusinessRegistration/Register';
-import PaymentInfo from './BusinessRegistration/PaymentInfo';
-import AddPhoto from './BusinessRegistration/AddPhoto';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+// import Register from './components/Register';
+import UserSignUp from './Components/UserSignUp/UserSignUp';
+import MainAcc from './Components/MainAcc/MainAcc';
+import AltContact from './Components/AltContact/AltContact';
+import Sign_In_Screen from './Components/Sign_In_Screen/Signinscreen';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Register/>} />
-      <Route path='/addPhoto' element={<AddPhoto/>} />
-      <Route path='/paymentInfo' element={<PaymentInfo/>} />
-    </Routes>
+      <Routes>
+        {/* <Route path='/' element={<Register />} /> */}
+        <Route path='/' element={<UserSignUp/>} />
+        <Route path='/mainacc' element={<MainAcc/>} />
+        <Route path='/altcontact' element={<AltContact/>} />
+        <Route path='/signinscreen' element={<Sign_In_Screen/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/Components/UserSignUp/UserSignUp.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./SignUp.css";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { FcNext } from "react-icons/fc";
+
+function UserSignUp() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if email and password are not empty
+    if (email.trim() === "" || password.trim() === "") {
+      alert("Please fill in all fields before signing in.");
+      return;
+    }
+
+    // Handle form submission logic here
+    console.log("Form submitted:", { email, password });
+
+    // Navigate to the /mainacc route programmatically
+    navigate("/mainacc");
+  };
+
+  const handleBusinessSignUp = () => {
+    // Navigate to the /businesssignup route programmatically
+    navigate("/businesssignup");
+  };
+
+  return (
+    <div className="background-container">
+      <div className="form-container">
+        <div className="form-item form-items">
+          <img src="./logo.png" alt="Logo" className="logo" />
+          <h2 className="signUp">Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            <label className="label">Email</label>
+            <input
+              type="email"
+              placeholder=""
+              className="form-group"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <label className="label">Password</label>
+            <input
+              type="password"
+              placeholder=""
+              className="form-group"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+
+            <button className="submit-button" type="submit">
+              SIGN UP
+            </button>
+
+            <a className="forgot-password" href="/signinscreen">ALREADY HAVE AN ACCOUNT?</a>
+
+
+            <div className="google-signin">
+              <a href="/">
+                <FcGoogle /> SIGN IN WITH GOOGLE
+              </a>
+            </div>
+          </form>
+          <button className="signup-business" onClick={handleBusinessSignUp}>
+            SIGN UP AS A BUSINESS <FcNext />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserSignUp;
