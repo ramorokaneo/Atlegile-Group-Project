ba376a889fa0a83e916924ce31cd959c69e0e254
Mahlatsek29 <serathikm@gmail.com>
2023-11-16 02:53:54 -0800

    shaun  - pasting files

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,51 +1,55 @@
-// // Footer.js
-// import React from "react";
-// import Footer from "../../componets/Footer/Footer";
-// import FollowUs from "../../componets/FollowUs/FollowUs";
-// import SaveBigHeader from "../../componets/SaveBigHeader/SaveBigHeader";
-// import AfricaBusines from "../../componets/AfricaBusiness/AfricaBusines";
-// import Navbar from "../../componets/NavBar/NavBar";
-// import SearchBar from "../../componets/SearchBar/SearchBar";
-// import Explore from "../../componets/Explore/Explore";
-// import SecureTech from "../../componets/SecureTech/SecureTech";
-// import LearnZa from "../../componets/LearnZa/LearnZa";
-// import TechWise from "../../componets/TechWise/TechWise";
-// import MindMatters from "../../componets/MindMatters/MindMatters";
-// // import AboutUs from "../../componets/AboutUs/AboutUs";
-// function Landing() {
-//   return (
-//     <>
-//       <FollowUs />
-//       <Navbar />
-//       <SearchBar />
-//       <Explore />
-//       <div
-//         style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
-//       >
-//         <SecureTech />
-//       </div>
-//       <SaveBigHeader />
-//       <div
-//         style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
-//       >
-//         <LearnZa />
-//       </div>
-//       <div
-//         style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
-//       >
-//         <TechWise />
-//       </div>
-//       <SaveBigHeader />
-//       <div
-//         style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
-//       >
-//         <MindMatters />
-//       </div>
-//       <AfricaBusines />
+// Footer.js
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import FollowUs from "../../components/FollowUs/FollowUs";
+import SaveBigHeader from "../../components/SaveBigHeader/SaveBigHeader";
+import AfricaBusines from "../../components/AfricaBusiness/AfricaBusines";
+import Navbar from "../../components/NavBar/NavBar";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Explore from "../../components/Explore/Explore";
+import SecureTech from "../../components/SecureTech/SecureTech";
+import LearnZa from "../../components/LearnZa/LearnZa";
+import TechWise from "../../components/TechWise/TechWise";
+import MindMatters from "../../components/MindMatters/MindMatters";
+// import AboutUs from "../../components/AboutUs/AboutUs";
+import { useFetchProducts } from "../../hooks/useFetchProducts";
 
-//       <Footer />
-//     </>
-//   );
-// }
+function Landing() {
+  const { firebaseProducts } = useFetchProducts();
+  console.log("firebase Products : ", firebaseProducts);
+  return (
+    <>
+      <FollowUs />
+      <Navbar />
+      <SearchBar />
+      <Explore />
+      <div
+        style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
+      >
+        <SecureTech data={firebaseProducts} />
+      </div>
+      <SaveBigHeader />
+      <div
+        style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
+      >
+        <LearnZa data={firebaseProducts} />
+      </div>
+      <div
+        style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
+      >
+        <TechWise data={firebaseProducts} />
+      </div>
+      <SaveBigHeader />
+      <div
+        style={{ paddingLeft: "20vw", display: "flex", alignItems: "center" }}
+      >
+        <MindMatters data={firebaseProducts} />
+      </div>
+      <AfricaBusines />
 
-// export default Landing;
+      <Footer />
+    </>
+  );
+}
+
+export default Landing;
