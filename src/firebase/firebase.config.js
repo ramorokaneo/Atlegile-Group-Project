2fdb7842b85244e9cbde2bd7a71abd0d9485cebb
Neo <ramoroka.neo@gmail.com>
2023-11-17 11:32:49 +0200

    Main latest change

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import 'firebase/compat/firestore';
+import firebase from 'firebase/compat/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,11 +19,16 @@ const firebaseConfig = {
   measurementId: "G-9Z66TT7GSR",
 };
 
+if (!firebase.apps.legnth) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const FIRESTORE_DB = getFirestore(app);
 // const storage =
 const auth = initializeAuth(app);
+const firestore = firebase.firestore();
 
 export { FIRESTORE_DB, auth };
