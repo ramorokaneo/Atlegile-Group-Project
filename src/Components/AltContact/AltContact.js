de4253af8322463fd64a1dfd6472ab8cca18e459
Malwandla Shaun Hlungwani <97354324+MalwandlaShaun@users.noreply.github.com>
2023-11-17 10:37:27 +0200

    Update AltContact.js

--- src/Components/AltContact/AltContact.js
+++ src/Components/AltContact/AltContact.js
@@ -3,12 +3,26 @@ import "./AltContact.css";
 import { useNavigate } from "react-router-dom";
 const logo = require("./Logo.png");
 
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { firebase, firestore } from "../../firebase/firebase.config";
+import { useFetchProfileData } from "../../hooks/useFetchUsers";
+
 function AltContact() {
+  const user = firebase.auth().currentUser;
+
+  const { userData } = useFetchProfileData(user.uid);
+
   const navigate = useNavigate();
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (name.trim() === "" || phone.trim() === "") {
@@ -16,17 +30,75 @@ function AltContact() {
       return;
     }
 
-    console.log("Form submitted:", { name, phone });
+    try {
+      // localStorage.setItem("user", user.uid);
 
-    navigate("/");
+      // const stringObj = localStorage.getItem("user");
+      // const parsedString = JSON.parse(stringObj);
+      // console.log("user : ", user);
+      const userRef = firestore.collection("Users").doc(user.uid);
+      await userRef.set({
+        name: userData.name,
+        surname: userData.surname,
+        phone: userData.phone,
+        gender: userData.gender,
+        email: userData.email,
+        location: userData.location,
+        uid: user.uid,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        alternativeContact: {
+          name,
+          phone,
+          timestamp: serverTimestamp(),
+        },
+      });
+      // await firestore
+      //   .collection("Users")
+      //   .doc(user.uid)
+      //   .set({
+      //     alternativeContact: {
+      //       name,
+      //       phone,
+      //       timestamp: serverTimestamp(),
+      //     },
+      //   });
+      // await updateDoc(
+      //   userRef,
+      //   {
+      //     name: localStorage.getItem("name"),
+      //     surname: localStorage.getItem("surname"),
+      //     phone: localStorage.getItem("phone"),
+      //     gender: localStorage.getItem("gender"),
+      //     email: localStorage.getItem("email"),
+      //     location: localStorage.getItem("location"),
+      //     alternativeContact: {
+      //       name,
+      //       phone,
+      //       timestamp: serverTimestamp(),
+      //     },
+      //   },
+      //   { merge: true }
+      // );
+
+      console.log(
+        "Alternative contact information submitted to 'Users' collection in Firestore."
+      );
+
+      navigate("/");
+    } catch (error) {
+      console.error(
+        "Error submitting alternative contact information:",
+        error.message
+      );
+      // alert("Error submitting alternative contact information. Please try again.");
+    }
   };
 
   const handleNotNow = () => {
-    alert("Thank you for signing up");
-
-
-    navigate("/usersignup");
+    console.log("Not Now button clicked");
   };
+
+
 
   return (
     <div
